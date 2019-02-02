b0e1f062eac6772d12521df3b6ea3b046bbfcfc7
Liadrinz <435438602@qq.com>
2019-02-02 16:19:55 +0800

    non-nullable start_time

--- Backend/data/models.py
+++ Backend/data/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from datetime import datetime
 
 # 为老师自动生成buptid
 def default_bupt_id():
