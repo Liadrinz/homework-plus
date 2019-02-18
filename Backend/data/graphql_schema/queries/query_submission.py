cf00cf4a9d903a8c807103b0ae447bdb54a838db
Liadrinz <435438602@qq.com>
2019-02-18 14:57:25 +0800

    1

--- Backend/data/graphql_schema/queries/query_submission.py
+++ Backend/data/graphql_schema/queries/query_submission.py
@@ -5,6 +5,7 @@ from data.graphql_schema.types import *
 class QuerySubmission(object):
 
     all_submissions = graphene.List(SubmissionType)
+    get_submissions_by_ids = graphene.List(SubmissionType, ids=graphene.List(of_type=graphene.Int))
     get_submissions_by_owners = graphene.List(SubmissionType, owners=graphene.List(of_type=graphene.Int))
     get_submissions_by_assignments = graphene.List(SubmissionType, assignments=graphene.List(of_type=graphene.Int))
     get_submissions_by_courses = graphene.List(SubmissionType, courses=graphene.List(of_type=graphene.Int))
@@ -12,6 +13,12 @@ class QuerySubmission(object):
     # specific query for submissions
     def resolve_all_submissions(self, info, **kwargs):
         return models.HWFSubmission.objects.all()
+
+    def resolve_get_submissions_by_ids(self, info, **kwargs):
+        result = models.models.Q(pk=None)
+        for pk in kwargs['ids']:
+            result = result | models.models.Q(pk=pk)
+        return models.HWFSubmission.objects.filter(result)
 
     def resolve_get_submissions_by_owners(self, info, **kwargs):
         result = models.models.Q(pk=None)
